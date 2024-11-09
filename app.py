524499415ac52e8e4edfa3bcf41220a68e58f147
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-11-09 19:55:23 +0530

    made changes in run.py

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ async def run_code():
     return jsonify({"result": result})
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Get the PORT from environment variable
+    app.run(debug=True, host='0.0.0.0', port=port)
